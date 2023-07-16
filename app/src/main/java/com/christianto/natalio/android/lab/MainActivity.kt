dd5a3094a5f3f6dfaea8f5104d4ea94449c0363d
Christianto Natalio Saleky <christianto@mamiteam.com>
2023-07-17 02:06:19 +0700

    Finished development

--- app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
@@ -3,13 +3,39 @@ package com.christianto.natalio.android.lab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.christianto.natalio.android.lab.ui.theme.AndroidLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -30,14 +56,158 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(8.dp),
+            shape = RoundedCornerShape(8.dp),
+            elevation = 4.dp
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider(modifier = Modifier.padding(vertical = 8.dp))
+                CreateUserInformation(
+                    "Walter White",
+                    "Chemistry Teacher / Drug Kingpin",
+                    "heisenberg"
+                )
+                Button(
+                    modifier = Modifier.padding(top = 8.dp),
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }
+                ) {
+                    Text(
+                        text = "Toggle Portfolio",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box {}
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AndroidLabTheme {
-        Greeting("Android")
+private fun CreateUserInformation(
+    name: String,
+    occupation: String,
+    handle: String
+) {
+    Column {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primary
+        )
+        Text(
+            text = occupation,
+            style = MaterialTheme.typography.subtitle1,
+        )
+        Text(
+            text = "@$handle",
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            modifier = modifier.size(135.dp),
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile Image",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .padding(top = 8.dp)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(6.dp)
+        ) {
+            Portfolio(data = listOf(
+                "Project 1",
+                "Project 2",
+                "Project 3",
+                "Project 4",
+                "Project 5",
+                "Project 6",
+                "Project 7"
+            ))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            ItemCard(item)
+        }
+    }
+}
+
+@Composable
+fun ItemCard(item: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .background(MaterialTheme.colors.surface)
+        ) {
+            CreateImageProfile(modifier = Modifier.size(100.dp))
+            Column(modifier = Modifier
+                .padding(start = 8.dp)
+                .align(alignment = Alignment.CenterVertically)
+            ) {
+                Text(text = item, fontWeight = FontWeight.Bold)
+                Text(text = "Meth Project", style = MaterialTheme.typography.body2)
+            }
+        }
     }
 }
