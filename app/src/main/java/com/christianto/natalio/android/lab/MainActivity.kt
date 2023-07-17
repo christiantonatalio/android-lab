a2252f807fb0e432aa739c9414e6d0261ba5dc47
Natalio Saleky <nataliosaleky@gmail.com>
2023-07-17 22:56:47 +0700

    Finished development

--- app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
@@ -3,13 +3,28 @@ package com.christianto.natalio.android.lab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.christianto.natalio.android.lab.ui.theme.AndroidLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +33,63 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidLabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                AppUI()
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun AppUI() {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            color = Color(0XFF546E7A)
+        ) {
+            val amountState = remember {
+                mutableStateOf(0)
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "$${amountState.value}",
+                    style = TextStyle(
+                        color = updateColorState(amountState.value),
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                AddCircleButton(amountState.value) {
+                    amountState.value = it
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun updateColorState(amountState: Int) =
+    if (amountState >= 1000) Color.Green else Color.White
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AndroidLabTheme {
-        Greeting("Android")
+fun AddCircleButton(amount: Int, updateAmount: (Int) -> Unit) {
+    Button(
+        modifier = Modifier
+            .padding(top = 32.dp)
+            .size(120.dp),
+        onClick = {
+            updateAmount(amount + 100)
+        },
+        shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White
+        )
+    ) {
+        Text("Tap")
     }
 }
