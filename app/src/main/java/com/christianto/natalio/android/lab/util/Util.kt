b152ccc246ab3600d259689a0ab22384f3b0ea3f
Natalio Saleky <nataliosaleky@gmail.com>
2023-07-20 21:40:59 +0700

    Added calculateTotalPerPerson Util

--- app/src/main/java/com/christianto/natalio/android/lab/util/Util.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/util/Util.kt
@@ -1,5 +1,11 @@
 package com.christianto.natalio.android.lab.util
 
-fun calculateTipAmount(bill: Double, percentage: Int): Double = if (bill > 1) {
+fun calculateTipAmount(bill: Double, percentage: Int) = if (bill > 1) {
     (bill * percentage) / 100
 } else 0.0
+
+fun calculateTotalPerPerson(
+    totalBill: Double,
+    splitBy: Int,
+    tipPercentage: Int
+) = (calculateTipAmount(totalBill, tipPercentage) + totalBill) / splitBy
