e52ef394d87f6ab61127c8497996450864d0a4cb
Natalio Saleky <nataliosaleky@gmail.com>
2023-07-21 10:18:57 +0700

    Updated default

--- app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
@@ -3,12 +3,10 @@ package com.christianto.natalio.android.lab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.christianto.natalio.android.lab.ui.theme.AndroidLabTheme
 
@@ -16,28 +14,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidLabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    AndroidLabTheme {
+        // A surface container using the 'background' color from the theme
+        content()
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        Text(text = "Hello Android Lab!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AndroidLabTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
