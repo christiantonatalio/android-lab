f7b2bae5594f2ad04f1dd7ebd4c12d897b1dee39
Christianto Natalio Saleky <christianto@mamiteam.com>
2023-07-27 11:27:18 +0700

    Added repository and added note database function inside the repository

---
+++ app/src/main/java/com/christianto/natalio/android/lab/repository/NoteRepository.kt
@@ -0,0 +1,24 @@
+package com.christianto.natalio.android.lab.repository
+
+import com.christianto.natalio.android.lab.data.NoteDatabaseDao
+import com.christianto.natalio.android.lab.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(
+    private val noteDatabaseDao: NoteDatabaseDao
+) {
+    suspend fun addNote(note: Note) = noteDatabaseDao.insert(note)
+
+    suspend fun updateNote(note: Note) = noteDatabaseDao.update(note)
+
+    suspend fun deleteNote(note: Note) = noteDatabaseDao.deleteNote(note)
+
+    suspend fun deleteAllNotes() = noteDatabaseDao.deleteAll()
+
+    fun getAllNotes(): Flow<List<Note>> =
+        noteDatabaseDao.getNotes().flowOn(Dispatchers.IO).conflate()
+}
