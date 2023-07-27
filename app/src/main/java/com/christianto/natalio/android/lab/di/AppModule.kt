d2a6b45ffc7d68271c8991d01950f275f6a3d636
Christianto Natalio Saleky <christianto@mamiteam.com>
2023-07-27 11:21:22 +0700

    Added note database in app module

--- app/src/main/java/com/christianto/natalio/android/lab/di/AppModule.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/di/AppModule.kt
@@ -1,11 +1,26 @@
 package com.christianto.natalio.android.lab.di
 
+import android.content.Context
+import androidx.room.Room
+import com.christianto.natalio.android.lab.data.NoteDatabase
+import com.christianto.natalio.android.lab.data.NoteDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase): NoteDatabaseDao = noteDatabase.noteDao()
 
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): NoteDatabase =
+        Room.databaseBuilder(context, NoteDatabase::class.java, "note_db")
+            .fallbackToDestructiveMigration().build()
 }
