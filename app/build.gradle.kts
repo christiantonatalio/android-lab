b77db4efe7b19a30efd6113fff0ca5410d4ed8fb
Christianto Natalio Saleky <christianto@mamiteam.com>
2023-08-01 19:29:57 +0700

    Finished migrated

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.christianto.natalio.android.lab"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.christianto.natalio.android.lab"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    fun Packaging.() {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.bundles.androidx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.androidx)
+    debugImplementation(libs.bundles.androidx)
+}
