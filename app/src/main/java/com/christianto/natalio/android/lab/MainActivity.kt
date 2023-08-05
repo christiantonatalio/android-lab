1fb8bf76b481dc47a1a109b17976fe10b0e9778c
Christianto Natalio Saleky <christianto@mamiteam.com>
2023-08-05 17:56:41 +0700

    Updated colors and fixed migration errors

--- app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
+++ app/src/main/java/com/christianto/natalio/android/lab/MainActivity.kt
@@ -3,9 +3,9 @@ package com.christianto.natalio.android.lab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.christianto.natalio.android.lab.ui.theme.AndroidLabTheme
@@ -32,7 +32,7 @@ fun MyApp(content: @Composable () -> Unit) {
 @Composable
 fun MainContent() {
     Surface(
-        color = MaterialTheme.colors.background
+        color = MaterialTheme.colorScheme.background
     ) {
         Text(text = "Hello Android Lab!")
     }
